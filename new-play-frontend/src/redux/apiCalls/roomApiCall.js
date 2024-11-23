d054220403dd319d16e6dfbd94fb19a35678cdbf
yassineelmiri <yassireelhima@gmail.com>
2024-11-23 18:41:21 +0100

    Ajout du fichier roomApiCall.js

---
+++ new-play-frontend/src/redux/apiCalls/roomApiCall.js
@@ -0,0 +1,18 @@
+import axios from "../../utils/axios";
+import {
+    fetchroomsStart, fetchroomsSuccess, fetchroomsFailure
+  } from "../slices/roomSlice.js";
+
+export const fetchrooms =  () => async (dispatch) => {
+    dispatch(fetchroomsStart());
+  try {
+    const response = await axios.get("/room");
+
+    dispatch(fetchroomsSuccess(response.data));
+    if (Array.isArray(response.data)) {
+      return response.data.rooms;
+    }
+  } catch (error) {
+    dispatch(fetchroomsFailure(error.message || "Failed to fetch user"));
+  }
+};
